1284a3114faa093c4bcd991ffcb5d6f1a38f011c
Vipin Kumar <vipul15kumar@gmail.com>
2024-07-20 19:44:59 +0530

    HashRouter added for github deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { ThemeProvider, styled } from "styled-components";
 import { darkTheme, lightTheme } from "./utils/Themes.js";
 import Navbar from "./components/Navbar";
